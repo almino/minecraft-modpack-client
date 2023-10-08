6158f3f90d0200b4cbe2938d9166f34be363f8f6
Almino Melo <almino.melo@gmail.com>
2023-10-08 16:06:30 -0300

    packwiz init

---
+++ shell.nix
@@ -0,0 +1,9 @@
+# https://github.com/nix-community/nix-direnv/blob/4132068df5153d44b7c17401ccefe54f99736dc1/README.md#usage-example
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  packages = with pkgs; [
+    adoptopenjdk-jre-bin
+    packwiz # https://packwiz.infra.link/
+  ];
+}
